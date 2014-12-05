ae922a3f79a04d29ccc611b4c61199d29b624802
timhua <tim.hua@icloud.com>
2014-12-04 20:10:58 -0800

    [fixed] merge error in lib/collection.js

--- leopardSpot/lib/collections.js
+++ leopardSpot/lib/collections.js
@@ -1,12 +1,7 @@
 //Create new Mongo Collection for slides
 SlideDeck = new Mongo.Collection('slides');
 Polling = new Mongo.Collection('polling');
-<<<<<<< HEAD
 PresentSession = new Mongo.Collection('PresentSession');
-
-=======
-PresentSession = new MongoCollection('PresentSession');
->>>>>>> 16846eda1da1b597b719d54fe9c1e80e355f8c2a
 
 // PresentSession = 
 // //slidedeck 
