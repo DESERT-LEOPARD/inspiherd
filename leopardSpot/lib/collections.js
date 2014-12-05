075013bf188073098dfda353c74c9f4d7bbe9723
timhua <tim.hua@icloud.com>
2014-12-04 16:51:31 -0800

    [done] initial collection and routes

---
+++ leopardSpot/lib/collections.js
@@ -0,0 +1,15 @@
+//Create new Mongo Collection for slides
+SlideDeck = new Mongo.Collection('slides');
+Polling = new Mongo.Collection('polling');
+PresentSession = new MongoCollection('PresentSession');
+
+// PresentSession = 
+// //slidedeck 
+// {
+//   owner: userId,
+//   title: 'string',
+//   mdRaw: 'string',
+//   mdSlides
+
+
+// }
